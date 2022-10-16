e33928affbaa3a96f84ea6ff051fc77181e4bf1f
Lovegiver <lovegiver@proton.me>
2022-10-16 12:05:14 +0200

    Initial commit with base MarketplaceApp module

---
+++ src/test/java/com/citizenweb/marketplace/marketplaceapp/MarketplaceAppApplicationTests.java
@@ -0,0 +1,15 @@
+package com.citizenweb.marketplace.marketplaceapp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
